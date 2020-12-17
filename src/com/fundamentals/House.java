eb986913ad9dff68fad05c044fa7bb0a8cbdc31e
Graham Stranimier <kcraiderh8tr@gmail.com>
2020-12-16 19:46:45 -0600

    Lesson 4 House Object and comments

---
+++ src/com/fundamentals/House.java
@@ -0,0 +1,11 @@
+package com.fundamentals;
+
+public class House {
+
+    String foundation;
+    String roofType;
+    String doorCover;
+    int windowSize;
+
+    public void doorOpenClose() {}
+}
--- src/com/fundamentals/Main.java
+++ src/com/fundamentals/Main.java
@@ -1,9 +1,15 @@
 package com.fundamentals;
 
+/*
+* This Main class is the entry point to the application
+* */
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// This prints Hello Java
         System.out.println("Hello Java");
+
+        House myHouse = new House();
+
     }
 }
